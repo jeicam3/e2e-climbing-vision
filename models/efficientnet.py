8e41e227a0a67816aa7040bc6bc23fe5c6b7a520
Maciej Jakubowski <jakubowskimaciej3@gmail.com>
2026-05-06 16:29:04 +0200

    Feat: add optional backbone freezing

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -1,14 +1,22 @@
 import torch.nn as nn
 from torchvision import models
 
-def get_climbing_model(num_classes=4, dropout_rate=0.6):
-    #bazowy model - EfficientNet-B0
-    model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)
+def get_climbing_model(num_classes=4, dropout_rate=0.6, freeze_backbone=False):
+    """Tworzy model EfficientNet-B0 z opcją zamrożenia wag warstw bazowych."""
+    # pobranie bazowego modelu z wagami ImageNet
+    weights = models.EfficientNet_B0_Weights.DEFAULT
+    model = models.efficientnet_b0(weights=weights)
     
-    # Wyciągnięcie liczby cech wejściowych do klasyfikatora
+    #opcjonalne zamrożenie wag warstw bazowych - do E2E
+    if freeze_backbone:
+        for param in model.parameters():
+            param.requires_grad = False
+    
+    # wyciągnięcie liczby cech wejściowych
     num_ftrs = model.classifier[1].in_features
     
-    #zamiana klasyfikatora na specyfinczy dla wykrywania chwytów
+    # nadpisanie klasyfikatora
+    # warstwy wewnątrz nn.Sequential domyślnie mają requires_grad=True
     model.classifier[1] = nn.Sequential(
         nn.Dropout(p=dropout_rate), 
         nn.Linear(num_ftrs, num_classes),
