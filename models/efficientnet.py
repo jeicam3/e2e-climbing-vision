82674878f8253f9e96e17286707b92415d3095be
Maciej Jakubowski <jakubowskimaciej3@gmail.com>
2026-05-07 14:50:46 +0200

    Feat: more flexible freezing mechanism

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -1,36 +1,25 @@
 import torch.nn as nn
 from torchvision import models
 
-def get_climbing_model(num_classes=4, dropout_rate=0.6, mode='fine_tune_top'):
-    """
-        mode (str): 
-            'freeze_all' -> Trenujemy tylko klasyfikator
-            'fine_tune_top' -> Klasyfikator + ostatni blok features[8]
-            'full_train' -> Wszystkie warstwy odblokowane
-    """
+def get_climbing_model(num_classes=4, dropout_rate=0.6, freeze_until_block=8):
     weights = models.EfficientNet_B0_Weights.DEFAULT
     model = models.efficientnet_b0(weights=weights)
     
-    #domyślnie wszystko zamrożone
-    for param in model.parameters():
-        param.requires_grad = False
-    
-    if mode == 'full_train':
-        for param in model.parameters():
-            param.requires_grad = True
-        
-    elif mode == 'fine_tune_top':
-        # odmrożony ostatni i przedostatni blok konwolucyjny
-        for param in model.features[8].parameters():
-            param.requires_grad = True
-        for param in model.features[7].parameters():
-            param.requires_grad = True
-
+    # features to bloki od 0 do 8
+    # 8 to ostatni blok przed klasyfikatorem
+    for i, block in enumerate(model.features):
+        if i < freeze_until_block:
+            for param in block.parameters():
+                param.requires_grad = False
+        else:
+            for param in block.parameters():
+                param.requires_grad = True
+                
+    # klasyfikator zawsze odblokowany
     num_ftrs = model.classifier[1].in_features
     model.classifier[1] = nn.Sequential(
         nn.Dropout(p=dropout_rate), 
         nn.Linear(num_ftrs, num_classes),
         nn.Sigmoid()
     )
-    
     return model
